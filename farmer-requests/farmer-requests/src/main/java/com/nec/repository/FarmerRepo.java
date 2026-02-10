b634b17cd4bd59e75a183c417dc58e195e51d746
Kuncha Karthik <kunchakarthik0@gmail.com>
2026-02-10 23:58:22 +0530

    first commit

--- farmer-requests/farmer-requests/src/main/java/com/nec/repository/FarmerRepo.java
+++
@@ -1,7 +0,0 @@
-package com.nec.repository;
-
-import com.nec.entity.Farmer;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface FarmerRepo extends JpaRepository<Farmer, String> {
-}
